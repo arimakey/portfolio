38e7b734e1f628c65cf3d430d26f4913937b70e3
Arima_key <127151745+ArimaKey@users.noreply.github.com>
2024-04-13 03:56:13 -0500

    problem: added new structure page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://arimakey.github.io/',
-	integrations: [mdx(), sitemap()],
+  site: 'https://arimakey.github.io/',
+  integrations: [mdx(), sitemap(), icon({
+    include: {
+      mdi: ["*"],
+      codicon: ["terminal-bash"], // Loads only Material Design Icon's "account" SVG
+    },
+  })]
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -5,8 +5,14 @@ const projects = defineCollection({
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
+		pubDate: z.string(),
 		heroImage: z.string().optional(),
-		updatedDate: z.coerce.date().optional(),
+		secondImage: z.string().optional(),
+		title_second_section: z.string().optional(),
+		content_second_section: z.string().optional(),
+		title_third_section: z.string().optional(),
+		content_third_section: z.array(z.string()).optional(),
+		tecnologies: z.array(z.string()).optional(),
 	}),
 });
 
