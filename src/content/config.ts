3db24c86268c7bbb31bbc6c5e6086b1e83ee1823
Arima_key <127151745+ArimaKey@users.noreply.github.com>
2024-04-14 12:26:47 -0500

    refactor: Project component

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,13 +6,7 @@ const projects = defineCollection({
 		title: z.string(),
 		description: z.string(),
 		pubDate: z.string(),
-		heroImage: z.string().optional(),
-		secondImage: z.string().optional(),
-		title_second_section: z.string().optional(),
-		content_second_section: z.string().optional(),
-		title_third_section: z.string().optional(),
-		content_third_section: z.array(z.string()).optional(),
-		tecnologies: z.array(z.string()).optional(),
+		heroImage: z.string()
 	}),
 });
 
