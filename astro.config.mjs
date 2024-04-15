60433ee4c96621a1a35daa2b3c49d8323ac1e4c7
Arima_key <127151745+ArimaKey@users.noreply.github.com>
2024-04-14 21:48:13 -0500

    solved: icons - transition api

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://arimakey.github.io/',
   trailingSlash: 'never',
   output: 'static',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), icon()],
   i18n: {
     defaultLocale: "en",
     locales: ["es", "en"],
